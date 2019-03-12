0a1d091ba0ba17c13b594cad9ea908ad6ca35a7e
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-03-11 19:39:09 -0700

    lab10

--- recursiveFibonacci.cpp
+++ recursiveFibonacci.cpp
@@ -18,16 +18,16 @@ FibonacciR::FibonacciR(const int &n):n_(&n) { }
 FibonacciR::~FibonacciR() {}
 
 int FibonacciR::Fibonacci(const int &n){
-    if(n==0) {
+    if(n == 0) {
         return 0;
     }
-    else if(n==1) {
+    else if(n == 1) {
         return 1;
     }
-    return Fibonacci(n-1) + Fibonacci(n-2);
+    return Fibonacci(n - 1) + Fibonacci(n - 2);
 }
 
 void FibonacciR::PrintFibonacci() {
-    int FibonaciNum=Fibonacci(*n_);
-    cout << *n_ << "the fibonaci number: " << FibonaciNum << endl;
+    int FibonacciNum=Fibonacci(*n_);
+    cout << *n_ << "the fibonaci number: " << FibonacciNum << endl;
 }
