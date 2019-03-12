cea4751fccc6df7c6e075eeee62b6682164c1e89
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-03-11 19:35:09 -0700

    lab10

--- recursiveFibonacci.cpp
+++ recursiveFibonacci.cpp
@@ -8,4 +8,26 @@
 **                  d
 *********************************************************************/
 #include "recursiveFibonacci.hpp"
+#include <iostream>
+git commit -am "lab10"
 
+FibonacciR::FibonacciR() {}
+
+FibonacciR::FibonacciR(const int &n):n_(&n) { }
+
+FibonacciR::~FibonacciR() {}
+
+int FibonacciR::Fibonacci(const int &n){
+    if(n==0) {
+        return 0;
+    }
+
+    23  	else if(n==1)
+        24  		return 1;
+    25  	return Fibonacci(n-1) + Fibonacci(n-2);
+    26  }
+27  void FibonacciR::PrintFibonacci(){
+    28  	int FibonaciNum=Fibonacci(*n_);
+    29  	std::cout<<*n_<<"th fibonaci number: "<<FibonaciNum<<std::endl;
+
+    30  }
--- recursiveFibonacci.hpp
+++ recursiveFibonacci.hpp
@@ -22,4 +22,3 @@ public:
 };
 
 #endif // RECURSIVEFIBONACCI_HPP
-git commit -am "lab10"
